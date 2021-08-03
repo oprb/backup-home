4e4eb06af1900acb15d09663b811a538392d0dbf
Ole Priebusch <opriebusch@web.de>
2021-08-03 13:53:05 +0200

    Remove Function Definitions

--- backup.sh
+++ backup.sh
@@ -7,38 +7,31 @@ BACKUP_DESTINATION="${MOUNTPOINT}/rdiff_backups/ole_home"
 
 RDIFF_INCLUDE_LIST="/home/ole/rdiff_backup_list"
 
-SLEEP_DURATION="10s"
+SLEEP_DURATION="2s"
 
-function is_backup_device_mounted () {
-  test "$(findmnt --raw --noheadings $BACKUP_DEVICE $MOUNTPOINT | cut --delimiter=' ' --fields=1,2)" \
-       = "$MOUNTPOINT $BACKUP_DEVICE"
-}
+# show a reminder that a backup will soon happen
+notify-send --urgency=critical --expire-time=5000 \
+            "Backup" "Backup will start soon. Make sure the backup device is connected."
 
-function show_backup_reminder () {
-  notify-send --urgency=critical --expire-time=5000 \
-             "Backup" "Backup will start soon. Make sure the backup device is connected."
-}
+sleep $SLEEP_DURATION
 
-function perform_backup () {
+# check if the backup device is mounted checking the findmnt output
+if [ "$(findmnt --raw --noheadings $BACKUP_DEVICE $MOUNTPOINT | cut --delimiter=' ' --fields=1,2)" \
+     = "$MOUNTPOINT $BACKUP_DEVICE" ]
+then
   echo "Backing up $BACKUP_SOURCE to $BACKUP_DESTINATION"
-  echo "Items backuped:"
-  cat $RDIFF_INCLUDE_LIST
-  rdiff-backup --include-globbing-filelist $RDIFF_INCLUDE_LIST $BACKUP_SOURCE $BACKUP_DESTINATION >&1
-}
 
-function backup () {
-  show_backup_reminder
-
-  sleep $SLEEP_DURATION
-
-  if is_backup_device_mounted
+  if [ ! -d $BACKUP_DESTINATION ]
   then
-    perform_backup
-    return
+    echo "Backup destination was not found. $BACKUP_DESTINATION will be created."
+    mkdir -p $BACKUP_DESTINATION
   fi
 
-  echo "No backup is performed since the backup device is not mounted." 
+  rdiff-backup --include-globbing-filelist $RDIFF_INCLUDE_LIST $BACKUP_SOURCE $BACKUP_DESTINATION
+  echo -e "Items backuped:\n" $RDIFF_INCLUDE_LIST
+else
+  echo "No backup is performed since the backup device is not mounted."
   notify-send --urgency=critical --expire-time=5000 \
               "Backup" "Backup device is not connected. Could not backup!"
   exit 1
-}
+fi
