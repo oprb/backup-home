543b15f0189414f74f673031703fe0f10d1bfac9
Ole Priebusch <opriebusch@web.de>
2021-08-04 12:52:24 +0200

    Make the Script User-Agnostic, Provide More Messages

--- backup.sh
+++ backup.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
+USER=$(whoami)
 BACKUP_DEVICE="/dev/sda1"
 MOUNTPOINT="/media/ole/backup"
-BACKUP_SOURCE="/home/ole"
-BACKUP_DESTINATION="${MOUNTPOINT}/rdiff_backups/ole_home"
+BACKUP_SOURCE="/home/${USER}"
+BACKUP_DESTINATION="${MOUNTPOINT}/rdiff_backups/${USER}_home"
 
-RDIFF_INCLUDE_LIST="/home/ole/rdiff_backup_list"
+RDIFF_EXCLUDE_LIST="/home/${USER}/rdiff_backup_list"
 
-SLEEP_DURATION="2s"
+SLEEP_DURATION="2m"
 
-# show a reminder that a backup will soon happen
+# show a reminder that a backup will happen soon
 notify-send --urgency=critical --expire-time=5000 \
             "Backup" "Backup will start soon. Make sure the backup device is connected."
 
 sleep $SLEEP_DURATION
 
-# check if the backup device is mounted checking the findmnt output
+# check if the backup device is mounted by checking the findmnt output
 if [ "$(findmnt --raw --noheadings $BACKUP_DEVICE $MOUNTPOINT | cut --delimiter=' ' --fields=1,2)" \
      = "$MOUNTPOINT $BACKUP_DEVICE" ]
 then
   echo "Backing up $BACKUP_SOURCE to $BACKUP_DESTINATION"
+  echo "The following items will be excluded from the backup:"
+  cat $RDIFF_EXCLUDE_LIST
 
   if [ ! -d $BACKUP_DESTINATION ]
   then
@@ -27,11 +30,26 @@ then
     mkdir -p $BACKUP_DESTINATION
   fi
 
-  rdiff-backup --include-globbing-filelist $RDIFF_INCLUDE_LIST $BACKUP_SOURCE $BACKUP_DESTINATION
-  echo -e "Items backuped:\n" $RDIFF_INCLUDE_LIST
+  echo "Backup will start now."
+  rdiff-backup --exclude-filelist $RDIFF_EXCLUDE_LIST $BACKUP_SOURCE $BACKUP_DESTINATION
+
+  # test for the return code of rdiff-backup
+  if [ "$?" = "0" ]
+  then
+    echo "$BACKUP_SOURCE has been backed up successfully."
+    notify-send --urgency=normal --expire-time=5000 \
+                "Backup" "$BACKUP_SOURCE has been backed up successfully."
+  else
+    echo "Error(s) occured while backing up!"
+    notify-send --urgency=critical --expire-time=5000 \
+                "Backup" "Error(s) occured while backing up!"
+    exit 1
+  fi
 else
   echo "No backup is performed since the backup device is not mounted."
   notify-send --urgency=critical --expire-time=5000 \
               "Backup" "Backup device is not connected. Could not backup!"
   exit 1
 fi
+
+exit 0
